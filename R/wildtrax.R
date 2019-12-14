70bff7f08674427a0528f95ee1d00f387a12a2be
Marcus Becker <mabecker@ualberta.ca>
2019-12-13 18:14:48 -0700

    add documentation

---
+++ R/wildtrax.R
@@ -0,0 +1,9 @@
+#' wildtrax: Data extraction from the Wildtrax platform in R.
+#'
+#' This package provides functionality to query data from the environmental sensor platform, Wildtrax.
+#'
+#' @author Marcus Becker \email{mabecker@ualberta.ca}
+#' @author Alex MacPhail \email{agmacpha@ualberta.ca}
+#' @docType package
+#' @name wildtrax
+"_PACKAGE"
