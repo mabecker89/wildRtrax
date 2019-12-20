bfc68a368fb1357c4de3e0307b5b8d3f204e6950
Marcus Becker <mabecker@ualberta.ca>
2019-12-20 12:57:58 -0700

    add R scripts for three new functions: wt_con, collect_table, and collect_cam_meta

---
+++ R/wt_con.R
@@ -0,0 +1,26 @@
+#' Establish Connection to Wildtrax PostgreSQL Database
+#'
+#' @param username A character string indicating the username for access the Wildtrax database
+#' @param password A character string indicating the password associated with the supplied username
+#' @import DBI
+#' @import RPostgreSQL
+#' @export
+#' @examples
+#' @return This function returns an S4 object of class PostgreSQLConnection
+#'
+#' @export
+#' @author Marcus Becker
+#' @author Alex MacPhail
+
+# Establish connection to Wildtrax database
+wt_con <- function(username, password){
+    con <- DBI::dbConnect(
+    drv = dbDriver("PostgreSQL"),
+    dbname = "wildtrax",
+    host = "prod.wildtrax.ca",
+    port = "5432",
+    user = user,
+    password = password
+  )
+  con
+}
