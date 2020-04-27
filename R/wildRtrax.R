b068a2219bd19ff621b5591881dfd29249dd4523
Alex <agmacpha@ualberta.ca>
2020-04-27 13:51:32 -0600

    grammer

--- R/wildRtrax.R
+++ R/wildRtrax.R
@@ -1,6 +1,6 @@
 #' wildRtrax: Queries, cleaning, analysis, and quality control of data from the WildTrax database in R
 #'
-#' This package provides a set of functions to query environmental sensor data from the WildTrax platform, as well as perform basic data cleaning, analysis, and quality control.
+#' This package provides a set of functions to query environmental sensor data from the WildTrax platform, as well as perform quality control, analytics and data summaries.
 #'
 #' @author Marcus Becker \email{mabecker@ualberta.ca}
 #' @author Alex MacPhail \email{agmacpha@ualberta.ca}
