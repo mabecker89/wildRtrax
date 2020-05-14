9c567fba98819eca12abdde62580f9db378d8b0f
Alex MacPhail <agmacpha@ualberta.ca>
2020-05-14 12:07:56 -0600

    FIXED wt_aru_scanner woop!
    Now includes columns for file size, audio file length - parallel clustering not super needed apparently!

--- R/wt_aru_scanner.R
+++ R/wt_aru_scanner.R
@@ -3,7 +3,7 @@
 #' @param path0
 #' @param pattern
 #'
-#' @import stringr base stats lubridate tuneR R.utils doParallel foreach tidyverse data.table
+#' @import stringr base stats lubridate tuneR R.utils tidyverse data.table
 #' @return
 #' @export
 #'
@@ -19,12 +19,7 @@ library(foreach)
 library(tidyverse)
 library(data.table)
 
-wt_aru_scanner <- function(path0, pattern, dfraw) {
-
-  #Set up progress bar and parallel backend to use many processors
-  pb<- txtProgressBar(min = 0, max = 100, style=3)
-  cores = detectCores()
-  cl <- makeCluster(cores[1]-1) #not to overlaod
+wt_aru_scanner <- function(path0, pattern) {
 
   dfraw <- as.data.frame(file.info(list.files(path = path0,
                                     pattern = pattern,
@@ -32,52 +27,54 @@ wt_aru_scanner <- function(path0, pattern, dfraw) {
                                     full.names = TRUE,
                                     include.dirs = FALSE))) #Create a dataframe that is a list of files defined by path; pattern is regex usually "\\.wac$|\\.wav$|\\.mp3$|\\.flac$"
 
+  pb1<- txtProgressBar(min = 0, max = nrow(dfraw), style=3, title = "Reading audio file lengths...")
+
   setDT(dfraw,keep.rownames = T) #Move rownames to column
 
   colnames(dfraw)[1] <- "Filepath" #Rename column to Filepath
 
   dfraw$size<-round(as.numeric(dfraw$size)/1000000,2) #Convert to megabytes
 
-  dfraw<-dfraw %>%
-  dplyr::select(Filepath,size)
+  dfraw <- dfraw %>%
+    dplyr::select(Filepath,size)
 
-  dfraw$Filepath<-as.character(dfraw$Filepath)
+  dfraw$Filepath <- as.character(dfraw$Filepath)
 
-  dfraw$Filename <- paste(basename(gsub("\\..*", "", dfraw$Filepath)),'.wav',sep='') #Get filename from the filepath
+  dfraw$Filename <- basename(gsub("\\..*", "", dfraw$Filepath)) #Get filename from the filepath
 
   dfraw$Station <- ifelse((str_detect(dfraw$Filename, "_0+1_")), #Create the station key; concatenate, dataset, site, station.
                         (str_split_fixed(dfraw$Filename, "_", n=3)),
                         (str_split_fixed(dfraw$Filename, "_", n=2)))
 
-  jd_mix<-function(x){
-    yday(as.Date(str_replace(str_sub(str_sub(x, -15), 1, str_length(str_sub(x, -15))-7),
+  dfraw$JD<-yday(as.Date(str_replace(str_sub(str_sub(dfraw$Filename, -15), 1, str_length(str_sub(dfraw$Filename, -15))-7),
                              "(\\d{4})(\\d{2})(\\d{2})$", "\\1-\\2-\\3"))) #Get the julian date (easier for data graphing imo)
-  }
-
-  dfraw$Julian_Date <- jd_mix(dfraw$Filename)
 
   dfraw$Time <- str_sub(dfraw$Filename, -6) #Get the time substring
-
-  dfraw[order(dfraw$Filename), ] #
 
   #Get year of the data
   dfraw$Year<-as.numeric(vapply(strsplit(as.character(dfraw$Filepath), "/"), `[`, 6, FUN.VALUE=character(1)))
 
-  #Creates an index of all the unique times of the dataframe for each temporal value. So day 1 midnight = 1, day 1 dawn = 4. That way you can pick from the 4th, 5th, recording of the day if you want which would corrrspond to tracking dawn, dusk, etc.
+  dfraw[order(dfraw$Filename), ] #Reorder the dataframe to accept the time index properly
+
+  #Creates an index of all the unique times of the dataframe for each date and time value. So day 1 midnight = 1, day 1 dawn = 4. That way you can pick from the 4th, 5th, recording of the day if you want which would corrrspond to tracking dawn, dusk, etc.
   dfraw$Time_index <- ave(paste(dfraw$Station, dfraw$Julian_Date, dfraw$Year, sep=""), paste(dfraw$Station, dfraw$Julian_Date, dfraw$Year, sep=""), FUN=seq_along)
 
   #Read all the audio files and return length in seconds; need to add sample rate here too eventually
-  for (i in dfraw$Filepath) {
-    tryCatch(t<-readWave(i, from=0, to=Inf, units="seconds"),error=function(e) {
-      msg<-conditionMessage(e)
-      print(paste0(msg, i, sep=' *** '))})
+  dfraw$length <-0
+
+  #Read all the audio files and return length in seconds; need to add sample rate here too eventually
+  for (i in 1:nrow(dfraw)) {
+    if(dfraw$size[i]>0) {
+      tryCatch(x<-readWave(dfraw$Filepath[i],from=0,to=Inf, units="seconds",header=T),
+               error=function(e2) {
+                 msg<-conditionMessage(e2)
+                 print(paste0(msg, i, sep=' *** '))})
+      dfraw$length[i]<-round(x$samples / x$sample.rate)
+      setTxtProgressBar(pb1,i)}
   }
 
-  #Wrap it up / shut down progress bar and parallel clustering
+  #Wrap it up / shut down progress bar
+  print(dfraw)
+  return(dfraw)
   close(pb)
-  stopCluster(cl)
-
 }
-
-
-
