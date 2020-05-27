38d4ac1893ba0571c7586f4fb3c11b1eb11c6c72
Alex MacPhail <agmacpha@ualberta.ca>
2020-05-27 10:49:04 -0600

    alex review - need to handle multiple filetype, separate wonky with filename conventions for SM3s (_0+1_). unnested lists of audio metadata

--- R/wt_aru_scanner.R
+++ R/wt_aru_scanner.R
@@ -1,116 +1,46 @@
-#' Scans directories of audio data and returns the filepath, filename, file size, date, time, station key, etc, from the filename string and through file.info
+#' Scans directories of audio data and returns the filepath, filename, file size, date, time, station key, sample rate, length and number of channels to be used as filters for wt_aru_assign or other uses
 #'
-#' @param path0
-#' @param pattern
+#' @param path
+#' @param file_type
 #'
-#' @import stringr base stats lubridate tuneR R.utils tidyverse data.table tools
+#' @import furr fs pipeR tibble stringr lubridate tidyverese bioacoustics soundecology seewave tuneR
 #' @return dfraw
 #' @export
 #'
-#' @examples z<-wt_aru_scanner('/volumes/budata/abmi/2019/01','\\.wav$|\\.wac$')
+#' @examples z<-wt_aru_scanner('/volumes/budata/abmi/2019/01','wav')
 #'
 #'
+----------------------------
 
-wt_aru_scanner <- function(path0, pattern) {
-
-  print('Scanning audio files... please wait...')
-  print('')
-
-  dfraw <- as.data.frame(file.info(list.files(path = path0,
-                                    pattern = pattern,
-                                    recursive = TRUE,
-                                    full.names = TRUE,
-                                    include.dirs = FALSE))) #Create a dataframe that is a list of files defined by path; pattern is regex usually "\\.wac$|\\.wav$|\\.mp3$|\\.flac$"
-
-  print('Audio files scanned... extracting metadata...')
-  print('')
-
-  pb1<- txtProgressBar(min = 0, max = nrow(dfraw), style=3, title = "Reading audio file lengths...")
-
-  setDT(dfraw,keep.rownames = T) #Move rownames to column
-
-  colnames(dfraw)[1] <- "Filepath" #Rename column to Filepath
-
-  dfraw$size<-round(as.numeric(dfraw$size)/1000000,2) #Convert to megabytes
-
-  dfraw <- dfraw %>%
-    dplyr::select(Filepath,size)
-
-  dfraw$Filepath <- as.character(dfraw$Filepath)
-
-  dfraw$Filename <- basename(gsub("\\..*", "", dfraw$Filepath)) #Get filename from the filepath
-
-  dfraw$Station_key <- ifelse((str_detect(dfraw$Filename, "_0+1_")), #Create the station key; concatenate, dataset, site, station.
-                        (str_split_fixed(dfraw$Filename, "_", n=3)),
-                        (str_split_fixed(dfraw$Filename, "_", n=2)))
-
-  dfraw$recording_date <- as.Date(str_replace(str_sub(str_sub(dfraw$Filename, -15), 1, str_length(str_sub(dfraw$Filename, -15))-7),
-                             "(\\d{4})(\\d{2})(\\d{2})$", "\\1-\\2-\\3")) #Get the date from the filename string
-
-  dfraw$Julian_Date <- yday(dfraw$recording_date) #Get the julian date (easier for data graphing imo)
-
-  dfraw$Time<-format(strptime(str_sub(dfraw$Filename,-6),format="%H%M%S"),"%H:%M:%S") #Get the time substring
-
-  #Get year of the data only works for standard filenames
-  dfraw$Year<-str_sub(str_extract(dfraw$Filename,'([^_]+)(?:_[^_]+)$'),1,4)
-
-  dfraw[order(dfraw$Filename), ] #Reorder the dataframe to accept the time index properly
-
-  #Creates an index of all the unique times of the dataframe for each date and time value. So day 1 midnight = 1, day 1 dawn = 4. That way you can pick from the 4th, 5th, recording of the day if you want which would corrrspond to tracking dawn, dusk, etc.
-  dfraw$Time_index <- ave(paste(dfraw$Station_key, dfraw$Julian_Date, dfraw$Year, sep=""), paste(dfraw$Station_key, dfraw$Julian_Date, dfraw$Year, sep=""), FUN=seq_along)
-
-  #Read all the audio files and return length in seconds; need to add sample rate here too eventually
-  dfraw$length <- 0
-  dfraw$samplerate <- 0
-  dfraw$channels <- 0
-
-  dfraw$type <- file_ext(dfraw$Filepath) #Get filetype
-
-  #Read all the audio files and return length in seconds; need to add sample rate here too eventually
-  for (i in 1:nrow(dfraw)) {
-    if(dfraw$size[i]>0) {
-      tryCatch(x<-readWave(dfraw$Filepath[i],from=0,to=Inf, units="seconds",header=T),
-               error=function(e2) {
-                 msg<-conditionMessage(e2)
-                 print(paste0(msg, i, sep=' *** '))})
-      dfraw$length[i]<-round(x$samples / x$sample.rate)
-      dfraw$samplerate[i]<-x$sample.rate
-      dfraw$channels[i]<-x$channels
-      setTxtProgressBar(pb1,i)}
-  }
-
-  #Wrap it up / shut down progress bar
-  write.csv(dfraw,'./test_wt_aru_scanner.csv')
-  close(pb1)
-  return(dfraw)
-}
-
-<<<<<<< HEAD
-=======
 # Alternative structure for scanner
 
-# A few new packages required:
+# All required packages
 library(furrr)
 library(fs)
 library(pipeR)
-library(tidyr)
 library(tibble)
-
-# Example directory for testing
-root <- "B:/ABMI/2019/01/ABMI-0409/ABMI-0409-NE"
+library(stringr)
+library(lubridate)
+library(tidyverse)
+library(bioacoustics)
+library(soundecology)
+library(seewave)
+library(tuneR)
 
 # For parallel processing:
 plan(multiprocess)
 
 wt_aru_scanner <- function(path, file_type) {
-
   # Convert to regex ... have to think this through more.
   if(file_type == "wav") {
-    ext <- "\\.wav$"
-  } else {
-    ext <- "\\.wav$|\\.wac$"
+    ext <- "wav"
+  } else if (file_type == "wac") {
+    ext <- "wac"
+  } else if (file_type == "flac") {
+    ext <- "flac"
+  } else (file_type == "mp3") {
+    ext <- "mp3"
   }
-
   dfraw <-
     # First list then retrieve file size
     dir_ls(path = path, recurse = TRUE, regexp = ext) %>>%
@@ -118,11 +48,11 @@ wt_aru_scanner <- function(path, file_type) {
     future_map_dbl(., .f = ~ file_size(.), .progress = TRUE) %>%
     enframe() %>%
     # Convert size to megabytes
-    mutate(size_Mb = round(value / 10e6, digits = 2)) %>%
+    mutate(size_Mb = round(value / 10e5, digits = 2)) %>%
     select(filepath = name, size_Mb) %>%
     mutate(filename = str_replace(basename(filepath), "\\..*", "")) %>%
     # Parse station key and recording date time
-    separate(filename, into = c("station_key", "recording_date_time"),
+    separate(filename, into = c("station_key", "recording_date_time"), #Need a conditional statement here to handle different conditions of file names e.g. SM3s have _0+1_
              sep = "_", extra = "merge", remove = FALSE) %>%
     mutate(recording_date_time = ymd_hms(recording_date_time),
            julian = yday(recording_date_time),
@@ -136,16 +66,32 @@ wt_aru_scanner <- function(path, file_type) {
     "Audio files scanned. Extracting metadata ..." %>>%
     mutate(data = future_map(.x = filepath,
                              .f = ~ readWave(., from = 0, to = Inf, units = "seconds", header = TRUE),
-                             .progress = TRUE),
+                             .progress = TRUE), ### See file_type options below for functions that can read wac and flac
            length_seconds = future_map(.x = data, .f = ~ round(.x$samples / .x$sample.rate)),
            sample_rate = future_map(.x = data, .f = ~ pluck(.x$sample.rate)),
            n_channels = future_map(.x = data, .f = ~ pluck(.x$channels))) %>%
     select(filepath, filename, size_Mb, station_key, recording_date_time, year, julian, time_index,
-           length_seconds:n_channels)
+           length_seconds:n_channels) %>%
+    unnest(c('length_seconds','sample_rate','n_channels')) #Stack lists
 
   return(dfraw)
-
 }
 
->>>>>>> 6aa1c08f84c561879347159d299953dcaf250dd0
+#For reading different filetypes
+read_wac(filepath) #in bioacoustics
+wac2flac(filepath,reverse=T) #in seewave
+#MP3
+r<-readMP3(filepath)
+writeWave(r,extensible=T)
+
+
+
+
+
+
+
+
+
+
+
 
