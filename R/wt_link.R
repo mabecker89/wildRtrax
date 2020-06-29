5cf25860df3e9679b96ec273f8d8c25d6f1e26a8
Alex MacPhail <agmacpha@ualberta.ca>
2020-06-29 10:17:46 -0600

    pushing wt_link to fit with new batch uploader. will provide more functionality soon

---
+++ R/wt_link.R
@@ -0,0 +1,21 @@
+#' Creates symlinks of subsetted raw audio to the appropraite final destination on the server. Operation is quiet - check the final destination folder for more information
+#'
+#' @param df #wt_aru_assign dataframe output or list of file paths
+#' @import base
+#'
+#' @examples x<-wt_link(df)
+#'
+
+wt_link<-function(df) {
+  for (i in 1:nrow(df)) {
+    base_output_directory[i] <- paste0('/Volumes/BUdata/wildtrax_loads','/',basename(df$filepath[i]))
+    message('*** Creating symlink to ', base_output_directory[i], ' ***')
+    # prepare command
+    command <- sprintf(' sudo ln -s "%s" "%s" ', df$filepath[i], base_output_directory[i])
+    # finally, execute the command
+    link<-tryCatch(system2(command),
+                   error=function(e){
+                     msg<-conditionMessage(e)
+                     print(paste0(msg2, ' for ', df$filepath[i]))
+                   })
+  }}
