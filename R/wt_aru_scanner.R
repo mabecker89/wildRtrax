0378dc268a6837966eca063d8700951fb4fee15c
Alex MacPhail <agmacpha@ualberta.ca>
2020-07-08 15:53:30 -0600

    pushing wt_scanner out for bug testing et al. using new read_audio to deal with multiple filetypes. separating location from date and time still needs some work

--- R/wt_aru_scanner.R
+++ R/wt_aru_scanner.R
@@ -27,6 +27,7 @@ library(soundecology)
 library(seewave)
 library(tuneR)
 library(tictoc)
+library(tools)
 
 # For parallel processing:
 plan(multisession)
@@ -42,44 +43,27 @@ wt_aru_scanner <- function(path, file_type) {
     mutate(size_Mb = round(value / 10e5, digits = 2)) %>%
     select(filepath = name, size_Mb) %>%
     mutate(filename = str_replace(basename(filepath), "\\..*", "")) %>%
-    # Parse station key and recording date time
-    separate(filename, into = c("station_key", "recording_date_time"),
-             sep = "_", extra = "merge", remove = FALSE) %>%
+    mutate(ftype = file_ext(filepath)) %>%
+    # Parse location and recording date time
+    separate(filename, into = c("location", "recording_date_time"), sep = "\\_|\\[_0+1_]", extra = "merge", remove = FALSE) %>%
     mutate(recording_date_time = ymd_hms(recording_date_time),
            julian = yday(recording_date_time),
            year = year(recording_date_time)) %>%
-    mutate(ftype = str_extract(filepath,'.{3}$')) %>%
-    arrange(station_key, recording_date_time) %>%
+    arrange(location, recording_date_time) %>%
     # Create time index
-    group_by(station_key, year, julian) %>%
+    group_by(location, year, julian) %>%
     mutate(time_index = row_number()) %>%
     ungroup() %>>%
     # Obtain metadata from audio files
     "Audio files scanned. Extracting metadata ..." %>>%
-  #WAV METADATA
-    mutate(data = future_map(.x = filepath,
-                             .f =  ~ readWave(.x, from = 0, to = Inf, units = "seconds", header = TRUE) #} if (ftype == 'wav') {
-                             # else if (ftype == 'wac') { ~ read_wac(.x) }
-                             # else if (ftype == 'mp3') { ~ readMP3(.x) }
-                             # else if (ftype == 'flac') { ~ wac2flac(.x, reverse=T) }
-                             # else {print('File type not supported for ', .x)},
-                             ,.progress = TRUE),
-           length_seconds = future_map(.x = data, .f = ~ round(length(.x@left) / .x@samp.rate,2)),
+    mutate(data = future_map(.x = filepath, .f = ~ read_audio(.x, from = 0, to = Inf), .progress = TRUE),
+           length_seconds = future_map(.x = data, .f = ~ pluck(round((length(.x@left) / .x@samp.rate), 2))),
            sample_rate = future_map(.x = data, .f = ~ pluck(.x@samp.rate)),
-           n_channels = future_map(.x = data, .f = ~ pluck(.x@stereo))) %>%
-    select(filepath, filename, size_Mb, station_key, recording_date_time, year, julian, time_index,
-           length_seconds:n_channels) %>%
-    unnest(c('length_seconds','sample_rate','n_channels')) #Stack lists
-  dfraw$n_channels <- ifelse(dfraw$n_channels == TRUE,2,1)
+           stereo = future_map(.x = data, .f = ~ pluck(.x@stereo))) %>%
+     select(filepath, filename, size_Mb, location, recording_date_time, year, julian, time_index, ftype, length_seconds, sample_rate, stereo) %>%
+     unnest(c('length_seconds', 'sample_rate', 'stereo'))
   return(as.data.frame(dfraw))
 }
-
-
-
-
-
-
-
 
 
 
